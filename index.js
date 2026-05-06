1d46ed7565114d39da119b50631642624a31f9ca
stitched-sand. <sandraservat@gmail.com.>
2026-05-06 18:07:36 -0400

    HamburgerMenu

---
+++ index.js
@@ -0,0 +1,11 @@
+<script>
+  const menu = document.querySelector(".menu");
+
+  function openMenu() {
+    menu.classList.add("menu--open");
+  }
+
+  function closeMenu() {
+    menu.classList.remove("menu--open");
+  }
+</script>
